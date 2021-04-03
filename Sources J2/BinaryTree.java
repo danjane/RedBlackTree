62b270954d091e578162d43e27dbb5dab089f9f5
danjane <jdtjane@gmail.com>
2021-04-03 11:23:50 +0200

    Work so far on implementation of a red-black binary tree using test driven development.
    Binary search tree: insert, find, delete.
    RB tree: blackHeights, isBlack, isRed, redViolation

---
+++ Sources J2/BinaryTree.java
@@ -0,0 +1,276 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryTree {
+
+    enum Colour{RED, BLACK}
+
+    static class Node {
+        int key;
+        Node left, right, parent;
+        Colour colour;
+
+        public Node(int data){
+            key = data;
+            left = right = parent = null;
+            colour = Colour.RED;
+        }
+    }
+
+    private Node root = null;
+
+    public boolean isEmpty() {
+        return root==null;
+    }
+
+    public void insert(int key) {
+        root = insertRecursive(root, key);
+    }
+
+    private Node insertRecursive(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+        }
+        else if (key < root.key) {
+            root.left = insertRecursive(root.left, key);
+            root.left.parent = root;
+        }
+        else {
+            root.right = insertRecursive(root.right, key);
+            root.right.parent = root;
+        }
+        return root;
+    }
+
+    public void inserts(int[] xs) {
+        for (int x : xs) {
+            insert(x);
+        }
+    }
+
+    public void insertRB(int key) {
+        root = insertRecursiveRB(root, key);
+        root.colour = Colour.BLACK;
+    }
+
+    public void insertsRB(int[] keys) {
+        for (int key : keys) {
+            insertRB(key);
+            if (key == 3) // TODO: refactor the recolouring step
+                root.right.colour = Colour.BLACK;
+        }
+    }
+
+    private Node insertRecursiveRB(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+/*          1. Every node is either red or black.
+            2. The root is black.
+            3. Every leaf (NIL) is black.
+            4. If a node is red, then both its children are black.
+            5. For each node, all simple paths from the node to descendant leaves contain the
+            same number of black nodes. */
+        }
+        else if (key < root.key) {
+            root.left = insertRecursiveRB(root.left, key);
+            root.left.parent = root;
+        }
+        else {
+            root.right = insertRecursiveRB(root.right, key);
+            root.right.parent = root;
+        }
+        return root;
+    }
+
+    public void delete(int key) {
+        root = deleteRecursive(root, key);
+    }
+
+    private Node deleteRecursive(Node root, int key) {
+        if (root == null)
+            throw new Empty();
+
+        if (key < root.key)
+            root.left = deleteRecursive(root.left, key);
+        else if (key > root.key)  //traverse right subtree
+            root.right = deleteRecursive(root.right, key);
+        else {
+            // node contains only one child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            else {
+                // DISASTER! Node has two children
+                root.key = minimumValueRecursive(root.right);
+                root.right = deleteRecursive(root.right, root.key);
+            }
+        }
+        return root;
+    }
+
+    private List<Integer> getValuesRecursive(Node node) {
+        if (node == null) return new ArrayList<>();
+        List<Integer> left_values = getValuesRecursive(node.left);
+        List<Integer> right_values = getValuesRecursive(node.right);
+        left_values.add(node.key);
+        left_values.addAll(right_values);
+        return left_values;
+    }
+
+    private List<Integer> getPreorderValuesRecursive(Node node) {
+        // Preorder (Root, Left, Right)
+        if (node == null) return new ArrayList<>();
+        List<Integer> values = new ArrayList<>(Collections.singletonList(node.key));
+        values.addAll(getPreorderValuesRecursive(node.left));
+        values.addAll(getPreorderValuesRecursive(node.right));
+        return values;
+    }
+
+    public int[] values() {
+        List<Integer> list = getValuesRecursive(root);
+        int[] vs = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            vs[i] = list.get(i);
+        return vs;
+    }
+
+    public int[] preorderValues() {
+        List<Integer> list = getPreorderValuesRecursive(root);
+        int[] vs = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            vs[i] = list.get(i);
+        return vs;
+    }
+
+    private int minimumValueRecursive(Node n){
+        if (n.left == null) return n.key;
+        else return minimumValueRecursive(n.left);
+    }
+
+    public int minimumValue() {
+        if (isEmpty())
+            throw new Empty();
+        return minimumValueRecursive(root);
+    }
+
+    private Node findRecursive(Node n, int k) {
+        if (n==null) return null;
+        else if (k < n.key) return findRecursive(n.left, k);
+        else if (k > n.key) return findRecursive(n.right, k);
+
+        return n;
+    }
+
+    public boolean find(int k) {
+        return findRecursive(root, k) != null;
+    }
+
+    public Colour rootColour() {
+        return root.colour;
+    }
+
+    public int[] blackHeights() {
+        List<Integer> list = getBlackHeightsRecursive(root);
+        int[] vs = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            vs[i] = list.get(i);
+        return vs;
+    }
+
+    private List<Integer> getBlackHeightsRecursive(Node node) {
+        if (node == null) return new ArrayList<>(Collections.singletonList(1));
+        List<Integer> bhs = getBlackHeightsRecursive(node.left);
+        bhs.addAll(getBlackHeightsRecursive(node.right));
+        if (isBlack(node))
+            for (int i = 0; i < bhs.size(); i++)
+                bhs.set(i, bhs.get(i) + 1);
+        return bhs;
+    }
+
+    private boolean isBlack(Node n) {
+        return n.colour.equals(Colour.BLACK);
+    }
+
+    private boolean isRed(Node n) {
+        return n.colour.equals(Colour.RED);
+    }
+
+    public boolean redNodeWithRedChild() {
+        Node possibleRed = redNodeWithRedChildRecursive(root, false);
+        return (possibleRed != null && isRed(possibleRed));
+    }
+
+    private Node redNodeWithRedChildRecursive(Node root, boolean redParent) {
+        if (root==null)
+            return null;
+
+        boolean redRoot = isRed(root);
+
+        if (redParent && redRoot)
+            return root;
+        else {
+            Node node = redNodeWithRedChildRecursive(root.left, redRoot);
+            if (node==null)
+                return redNodeWithRedChildRecursive(root.right, redRoot);
+            return node;
+        }
+    }
+
+    private void leftRotate(Node node_x) {
+        Node node_y = node_x.right;
+        node_y.parent = node_x.parent;
+
+        node_x.right = node_y.left;
+        node_x.right.parent = node_x;
+
+        node_y.left = node_x;
+        node_x.parent = node_y;
+
+        if (node_y.parent == null)
+            root = node_y;
+    }
+
+    private void rightRotate(Node node_y) {
+        Node node_x = node_y.left;
+        node_x.parent = node_y.parent;
+
+        node_y.left = node_x.right;
+        node_y.left.parent = node_y;
+
+        node_x.right = node_y;
+        node_y.parent = node_x;
+
+        if (node_x.parent == null)
+            root = node_x;
+    }
+
+    public void leftRotateOnKey(int key) {
+        // For debugging, to allow a test of leftRotate
+        Node node = findRecursive(root, key);
+        leftRotate(node);
+    }
+
+    public void rightRotateOnKey(int key) {
+        // For debugging, to allow a test of rightRotate
+        Node node = findRecursive(root, key);
+        rightRotate(node);
+    }
+
+
+    static class Empty extends RuntimeException {
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        // tree.inserts(new int[] {11,2,14,1,7,15,5,8,4});
+        tree.inserts(new int[]{2,1,4,3,5});
+        tree.leftRotateOnKey(2);
+        int[] vs = tree.preorderValues();
+        for (int v : vs) {
+            System.out.println("This is : " + v);
+        }
+    }
+
+}
---
+++ Test/BinaryTreeTest.java
@@ -0,0 +1,192 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+    private BinaryTree tree;
+
+    @Before
+    public void setUp() {
+        tree = new BinaryTree();
+    }
+
+    @Test
+    public void newTree_isEmpty() {
+        assertTrue(tree.isEmpty());
+    }
+
+    @Test
+    public void addValueToNewTree_notEmpty() {
+        tree.insert(1);
+        assertFalse(tree.isEmpty());
+    }
+
+    @Test
+    public void newTree_valuesIsAnEmptyList() {
+        int[] empty_list = new int[0];
+        assertArrayEquals(empty_list, tree.values());
+    }
+
+    @Test
+    public void addOneValues_valuesIsThisValue() {
+        int[] values = new int[] {1, 1};
+        tree.inserts(values);
+        assertArrayEquals(values, tree.values());
+    }
+
+    @Test(expected = BinaryTree.Empty.class)
+    public void willThrowEmpty_WhenAskedForMinimum() {
+        tree.minimumValue();
+    }
+
+    @Test
+    public void add123_minimumIs1() {
+        tree.inserts(new int[] {1, 2, 3});
+        assertEquals(1, tree.minimumValue());
+    }
+
+    @Test
+    public void add213_minimumIs1() {
+        tree.inserts(new int[] {2, 1, 3});
+        assertEquals(1, tree.minimumValue());
+    }
+
+    @Test
+    public void newTree_returnsFalseOnFind1() {
+        assertFalse(tree.find(1));
+    }
+
+    @Test
+    public void add1_returnsTrueOnFind1() {
+        tree.insert(1);
+        assertTrue(tree.find(1));
+    }
+
+    @Test
+    public void add1_returnsFalseOnFind2() {
+        tree.insert(1);
+        assertFalse(tree.find(2));
+    }
+
+    @Test
+    public void add123_returnsTrueOnFind3() {
+        tree.inserts(new int[] {2, 1, 3});
+        assertTrue(tree.find(3));
+    }
+
+    @Test(expected = BinaryTree.Empty.class)
+    public void newTreeDelete1_throwsEmpty() {
+        tree.delete(1);
+    }
+
+    @Test
+    public void add21_delete1() {
+        tree.insert(2);
+        tree.insert(1);
+        tree.delete(1);
+    }
+
+    @Test
+    public void add1Delete1_isEmpty() {
+        tree.insert(1);
+        tree.delete(1);
+        assertTrue(tree.isEmpty());
+    }
+
+    @Test
+    public void add123and4delete4_valuesAre123() {
+        int[] values = new int[]{1, 2, 3};
+        tree.inserts(values);
+        tree.insert(4);
+        tree.delete(4);
+        assertArrayEquals(values, tree.values());
+    }
+
+    @Test
+    public void add123delete2_valuesAre13() {
+        tree.inserts(new int[]{1,2,3});
+        tree.delete(2);
+        assertArrayEquals(new int[]{1,3}, tree.values());
+    }
+
+    @Test
+    public void add321delete2_valuesAre13() {
+        tree.inserts(new int[]{3,2,1});
+        tree.delete(2);
+        assertArrayEquals(new int[]{1,3}, tree.values());
+    }
+
+    @Test
+    public void add231delete2_valuesAre13() {
+        tree.inserts(new int[]{2,3,1});
+        tree.delete(2);
+        assertArrayEquals(new int[]{1,3}, tree.values());
+    }
+
+    @Test
+    public void add1_isRed() {
+        tree.insert(1);
+        assertEquals(BinaryTree.Colour.RED, tree.rootColour());
+    }
+
+    @Test
+    public void add12_blackHeightsAre1() {
+        tree.insert(1);
+        tree.insert(2);
+        assertArrayEquals(new int[]{1,1,1}, tree.blackHeights());
+    }
+
+    @Test
+    public void add123_redNodeHasRedChild() {
+        tree.inserts(new int[]{1, 2, 3});
+        assertTrue(tree.redNodeWithRedChild());
+    }
+
+    @Test
+    public void insert1_isBlack() {
+        tree.insertRB(1);
+        assertEquals(BinaryTree.Colour.BLACK, tree.rootColour());
+    }
+
+    @Test
+    public void inserts12_blackHeightsAre1() {
+        tree.insertsRB(new int[]{1,2});
+        assertArrayEquals(new int[]{2,2,2}, tree.blackHeights());
+    }
+
+    @Test
+    public void add123_redNodeHasBlackChild() {
+        tree.insertsRB(new int[]{1, 2, 3});
+        assertFalse(tree.redNodeWithRedChild());
+    }
+
+    @Test
+    public void add21435_values12345() {
+        tree.inserts(new int[]{2,1,4,3,5});
+        assertArrayEquals(new int[]{1,2,3,4,5}, tree.values());
+    }
+
+    @Test
+    public void add21435_preorderValues12345() {
+        tree.inserts(new int[]{2,1,4,3,5});
+        assertArrayEquals(new int[]{2,1,4,3,5}, tree.preorderValues());
+    }
+
+    @Test
+    public void add21435LeftRotate_is42135() {
+        tree.inserts(new int[]{2,1,4,3,5});
+        tree.leftRotateOnKey(2);
+        assertArrayEquals(new int[]{4,2,1,3,5}, tree.preorderValues());
+    }
+
+    @Test
+    public void add21435LeftRightRotate_is21435() {
+        int[] values = new int[]{2,1,4,3,5};
+        tree.inserts(values);
+        tree.leftRotateOnKey(2);
+        tree.rightRotateOnKey(4);
+        assertArrayEquals(values, tree.preorderValues());
+    }
+}

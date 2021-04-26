cb08dc3494842a4f55b149268de22aae0d608f99
danjane <jdtjane@gmail.com>
2021-04-26 18:35:31 +0200

    Code to run the loop and record results in a csv

--- Sources J2/TreeTimer.java
+++ Sources J2/TreeTimer.java
@@ -1,7 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Stack;
+import java.lang.Math;
 
 public class TreeTimer {
 
@@ -13,7 +17,7 @@ public class TreeTimer {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < size; i++)
             list.add(i);
-        java.util.Collections.shuffle(list,  new Random(0));
+        //java.util.Collections.shuffle(list,  new Random(0));
 
         Stack<Integer> stack = new Stack<>();
         for (Integer e: list)
@@ -55,24 +59,38 @@ public class TreeTimer {
 
         long millis = (endTime - startTime) / 1000000;
 
-        // tree.print(); // too big
-        if (!checkTree(tree))
-            throw new RuntimeException("Tree fail with size" + size);
+        //if (!checkTree(tree))
+        //    throw new RuntimeException("Tree fail with size" + size);
 
         print("Time taken: " + millis + " ms");
         return millis;
     }
 
-    public static StringBuilder appendResult(StringBuilder sb, int size, int time) {
+    public static StringBuilder appendResult(StringBuilder sb, int size, long time) {
         sb.append(size).append(",").append(time).append("\n");
         return sb;
     }
 
 
     public static void main(String[] args) {
-        int size = 1000000;
+        StringBuilder sb = new StringBuilder("Size,Time\n");
+
+        for (int loop : new int[] {1,2,3,4,5,6}) {
+            for (int exponent : new int[] {14,15,16,17,18,19,20}) {
+                int size = (int) Math.pow(2, exponent);
+                appendResult(sb, size, timeSetOfInsertions(size));
+            }
+        }
 
 
+        try (PrintWriter writer = new PrintWriter(new File("test.csv"))) {
+            writer.write(sb.toString());
+
+            System.out.println("done!");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
